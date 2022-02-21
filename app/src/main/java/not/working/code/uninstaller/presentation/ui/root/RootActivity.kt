c6b71bdd1430ee88cda624fd61b3b58d2be02abb
NotWorkingCode <not.working.code@gmail.com>
2022-02-21 08:43:27 +0300

    Navigation configure

---
+++ app/src/main/java/not/working/code/uninstaller/presentation/ui/root/RootActivity.kt
@@ -0,0 +1,7 @@
+package not.working.code.uninstaller.presentation.ui.root
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import not.working.code.uninstaller.R
+
+class RootActivity : AppCompatActivity(R.layout.activity_root)
