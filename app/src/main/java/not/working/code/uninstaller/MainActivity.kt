eea57c46f1f57631f4502e3d2b2ac68275965d2a
NotWorkingCode <not.working.code@gmail.com>
2022-02-21 08:09:19 +0300

    Init

---
+++ app/src/main/java/not/working/code/uninstaller/MainActivity.kt
@@ -0,0 +1,11 @@
+package not.working.code.uninstaller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
