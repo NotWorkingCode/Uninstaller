5edf8392cb90a18756df51aab20e53f2d5d09a7d
NotWorkingCode <not.working.code@gmail.com>
2022-02-21 08:49:37 +0300

    Add MainFragment and FragmentToolbar

---
+++ app/src/main/java/not/working/code/uninstaller/presentation/ui/main/MainFragment.kt
@@ -0,0 +1,4 @@
+package not.working.code.uninstaller.presentation.ui.main
+
+class MainFragment {
+}
---
+++ app/src/main/java/not/working/code/uninstaller/presentation/ui/main/MainViewModel.kt
@@ -0,0 +1,9 @@
+package not.working.code.uninstaller.presentation.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel(
+
+) : ViewModel() {
+
+}
